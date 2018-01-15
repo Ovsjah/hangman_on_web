dd2eb7fafd7d227350233c6c815a77f9fb57e3ec
Ovsjah <ovsjah@gmail.com>
2018-01-15 15:35:14 +0200

    Complete project

---
+++ lib/secret_word.rb
@@ -0,0 +1,23 @@
+class SecretWord
+  attr_accessor :found_letters, :secret_word
+  
+  def initialize
+    @found_letters = []
+    @secret_word = create
+  end
+  
+  def create
+    arr_of_words = File.readlines("./static/5desk.txt")
+    words = arr_of_words.delete_if { |word| !word.strip.size.between?(5, 12) }
+    words.sample.strip.downcase
+  end
+  
+  def modify(letter = nil)
+    found_letters << letter unless letter.nil?
+    letters = found_letters.join
+    not_found_letters = secret_word.delete(letters)
+    secret_word.gsub(/[#{not_found_letters}]/, '_ ')
+  rescue RegexpError
+    "All letters are found! Well done!"
+  end    
+end
